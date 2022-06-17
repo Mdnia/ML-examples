07b2609617cb40dc99556443e097fa2f0e68352a
mdnia <mdnia49@gmail.com>
2022-06-17 13:19:15 +0200

    Vision transformers done

--- Vision transformers/vision_transformers.py
+++ Vision transformers/vision_transformers.py
@@ -2,13 +2,16 @@ import tensorflow as tf
 from tensorflow.keras import layers
 import matplotlib.pyplot as plt
 import numpy as np
+import pandas as pd
+
 
 print(tf.__version__)
 # mlp
 # Class patches
 # # Class patch_encoder
 # create_vit_classifier
-
+INPUT_SHAPE = (32, 32, 3)
+NUM_CLASSES = 100
 (x_train, y_train), (x_test, y_test) = tf.keras.datasets.cifar100.load_data()
 print(f"X-train shape: {x_train.shape}, y_train shape: {y_train.shape}")
 print(f"x_test shape: {x_test.shape}, y_test shape: {y_test.shape}")
@@ -18,7 +21,17 @@ IMAGE_SIZE = 72
 PATCH_SIZE = 6
 NUM_PATCHES = (IMAGE_SIZE // PATCH_SIZE) ** 2
 NUM_HEADS = 4
-
+PROJECTION_DIM = 64
+TRANSFORMER_LAYERS = 8
+TRANSFORMER_UNITS = [
+    PROJECTION_DIM * 2,
+    PROJECTION_DIM
+]
+BATCH_SIZE = 256
+NUM_EPOCHS = 100
+WEIGHT_DECAY = 0.0001
+LEARNING_RATE = 0.001
+MLP_HEAD_UNITS = [2048, 1024]
 
 def mlp(input, units, dropout_rate):
     for unit in units:
@@ -38,13 +51,14 @@ data_augmentation = tf.keras.Sequential([
     ),
 ], name='data_augmentation')
 
+data_augmentation.layers[0].adapt(x_train)
 
 class Patch_split(layers.Layer):
     def __init__(self, patch_size):
         super(Patch_split, self).__init__()
         self.patch_size = patch_size
 
-    def call(self, images):
+    def __call__(self, images):
 
         batch_size = tf.shape(images)[0]
         patches = tf.image.extract_patches(
@@ -67,22 +81,80 @@ class PatchEmbedding(layers.Layer):
             input_dim=num_patches, output_dim=projection_dim
         )
 
-    def call(self, patch):
-        positions =tf.range(start=0, limit=self.num_patches, delta=1)
+    def __call__(self, patch):
+        positions = tf.range(start=0, limit=self.num_patches, delta=1)
         encoded_patches = self.projection(patch) + self.positional_embedding(positions)
         return encoded_patches
 
 
 def embedded_patches():
 
+    return
 
 def transformer_classifier():
+    inputs = layers.Input(shape=INPUT_SHAPE)
+    augmented = data_augmentation(inputs)
+    patches = Patch_split(PATCH_SIZE)(augmented)
+    embedding = PatchEmbedding(NUM_PATCHES, PROJECTION_DIM)(patches)
 
+    for _ in range(TRANSFORMER_LAYERS):
+        norm1 = layers.LayerNormalization(epsilon=1e-6)(embedding)
+        attention = layers.MultiHeadAttention(num_heads=NUM_HEADS,
+                                              key_dim=PROJECTION_DIM, dropout=0.1)(norm1, norm1)
+        residual1 = layers.add([embedding, attention])
+        norm2 = layers.LayerNormalization(epsilon=1e-6)(residual1)
+        fc = mlp(norm2, units=TRANSFORMER_UNITS, dropout_rate=0.1)
+        encoded_patches = layers.add([residual1, fc])
+
+    end_norm = layers.LayerNormalization(epsilon=1e-6)(encoded_patches)
+    flatten = layers.Flatten()(end_norm)
+    dropout = layers.Dropout(0.5)(flatten)
+
+    features = mlp(dropout, units=MLP_HEAD_UNITS, dropout_rate=0.5)
+
+    logits = layers.Dense(NUM_CLASSES)(features)
+
+    model = tf.keras.Model(inputs=inputs, outputs=logits)
+
+    return model
+
+
+def train_model(model):
+
+
+
+    model.compile(loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
+                  optimizer=tf.keras.optimizers.Adam(lr=LEARNING_RATE),
+                  metrics=[
+                      tf.keras.metrics.SparseCategoricalAccuracy(name='accuracy'),
+                      tf.keras.metrics.SparseTopKCategoricalAccuracy(5, name='top-5-accuracy')
+                  ])
+
+    checkpoint_path = 'tmp/checkpoints'
+    checkpoint_callback = tf.keras.callbacks.ModelCheckpoint(
+        checkpoint_path,
+        monitor='val_accuracy',
+        save_best_only=True,
+        save_weights_only=True
+    )
+
+    history = model.fit(x=x_train,
+                        y=y_train,
+                        #validation_data=(x_test, y_test),
+                        epochs=NUM_EPOCHS,
+                        validation_split=0.1,
+                        callbacks=[checkpoint_callback])
+    model.load_weights(checkpoint_path)
+    _, accuracy, top_5_accuracy = model.evaluate(x_test, y_test)
+    print(f'Test accuracy: {round(accuracy * 100, 2)}%')
+    print(f'Top 5 test accuracy: {round(top_5_accuracy * 100, 2)}%')
+
+
+    return history
 
 
 if __name__ == '__main__':
-    plt.figure(figsize=(4, 4))
-    image = x_train[np.random.choice(range(x_train.shape[0]))]
-    plt.imshow(image.astype('uint8'))
-    plt.axis('off')
+    model = transformer_classifier()
+    history = train_model(model)
+    pd.DataFrame(history.history).plot(figsize=(10, 7))
     plt.show()
